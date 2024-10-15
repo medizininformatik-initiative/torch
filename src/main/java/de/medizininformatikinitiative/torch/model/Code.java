c4f831757a3d7e4df753e27e9786f7db66d03125
Bastian Schaffer <bastianschaffer.it@gmail.com>
2024-10-15 17:58:34 +0200

    Add Expand Code Concept Filter
    Fixes: #71
    
    Also had to change code 789012 in the tests to something else because this code does not appear in the mapping tree.

--- src/main/java/de/medizininformatikinitiative/torch/model/Code.java
+++ src/main/java/de/medizininformatikinitiative/torch/model/Code.java
@@ -2,9 +2,13 @@ package de.medizininformatikinitiative.torch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import de.medizininformatikinitiative.torch.config.SpringContext;
+import de.numcodex.sq2cql.model.common.TermCode;
+import de.numcodex.sq2cql.model.structured_query.ContextualTermCode;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Code {
@@ -12,6 +16,13 @@ public class Code {
     // No-argument constructor
     public Code() {
     }
+
+    // Constructor for direct instantiation (for testing)
+    public Code(String system, String code) {
+        this.system = system;
+        this.code = code;
+    }
+
     @JsonProperty("code")
     private String code;
 
@@ -22,6 +33,13 @@ public class Code {
     private String display;
 
 
+    public static Code of(ContextualTermCode t) {
+        Code codeObj = new Code();
+        codeObj.system = t.termCode().system();
+        codeObj.code = t.termCode().code();
+        codeObj.display = t.termCode().display();
+        return codeObj;
+    }
 
     public String getCodeURL(){
         String encodedString = "";
@@ -32,6 +50,12 @@ public class Code {
         }
         return encodedString;
 
+    }
+
+    public ContextualTermCode getContextualTermCode() {
+        var context = SpringContext.getCodeSystemToContextMapping().getContext(system, code);
+        var termCode = TermCode.of(system, code, "");
+        return ContextualTermCode.of(context, termCode);
     }
 
     // Getters and Setters
