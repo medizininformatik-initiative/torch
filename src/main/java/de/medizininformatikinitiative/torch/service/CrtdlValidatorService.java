770e413ee45e27b7a5f3f8030c5e4fba5cb06bfb
Lucas Triefenbach <zoo.triefenbach@gmx.de>
2025-01-13 14:41:52 +0100

    cleanup modifier adding and added compartment check

--- src/main/java/de/medizininformatikinitiative/torch/service/CrtdlValidatorService.java
+++ src/main/java/de/medizininformatikinitiative/torch/service/CrtdlValidatorService.java
@@ -1,25 +1,27 @@
 package de.medizininformatikinitiative.torch.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.medizininformatikinitiative.torch.management.StructureDefinitionHandler;
-import de.medizininformatikinitiative.torch.model.crtdl.AttributeGroup;
 import de.medizininformatikinitiative.torch.model.crtdl.Crtdl;
 import org.hl7.fhir.r4.model.ElementDefinition;
 import org.hl7.fhir.r4.model.StructureDefinition;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class CrtdlValidatorService {
 
-    private StructureDefinitionHandler profileHandler;
-    private final AttributeGroupPopulator attributeGroupPopulator;
+    private final StructureDefinitionHandler profileHandler;
+    private final Set<String> codes;
 
 
-    public CrtdlValidatorService(StructureDefinitionHandler profileHandler) {
+    public CrtdlValidatorService(StructureDefinitionHandler profileHandler) throws IOException {
         this.profileHandler = profileHandler;
-        this.attributeGroupPopulator = new AttributeGroupPopulator(profileHandler);
+        this.codes = extractCodes("compartmentdefinition-patient.json");
     }
 
     /**
@@ -35,23 +37,54 @@ public class CrtdlValidatorService {
     public void validate(Crtdl crtdl) {
         Set<String> profiles = new HashSet<>();
         profiles.addAll(profileHandler.knownProfiles());
-        List<AttributeGroup> groupList = new ArrayList<>();
 
 
         crtdl.dataExtraction().attributeGroups().forEach(attributeGroup -> {
             StructureDefinition definition = profileHandler.getDefinition(attributeGroup.groupReference());
             if (definition != null) {
-                attributeGroup.attributes().forEach(attribute -> {
-                    ElementDefinition elementDefinition = definition.getSnapshot().getElementById(attribute.attributeRef());
-                    if (elementDefinition == null) {
-                        throw new IllegalArgumentException("Unknown Attributes in " + attributeGroup.groupReference());
-                    }
-                });
+                if (!attributeGroup.includeReferenceOnly() && codes.contains(definition.getType())) {
+                    attributeGroup.attributes().forEach(attribute -> {
+                        ElementDefinition elementDefinition = definition.getSnapshot().getElementById(attribute.attributeRef());
+                        if (elementDefinition == null) {
+                            throw new IllegalArgumentException("Unknown Attributes in " + attributeGroup.groupReference());
+                        }
+                    });
 
+                } else {
+                    throw new IllegalArgumentException("Attribute would lead to direct extraction of a Resources outside the patient compartment " + attributeGroup.groupReference());
+                }
             } else {
                 throw new IllegalArgumentException("Unknown Profile: " + attributeGroup.groupReference());
             }
         });
+    }
+
+    /**
+     * Reads a JSON file from the resources folder, extracts codes in the resource fields, and returns them as a list.
+     * Used to read the compartment definition.
+     *
+     * @param fileName The name of the JSON file in the resources' folder.
+     * @return A Set of extracted codes.
+     * @throws IOException If the file cannot be read.
+     */
+    public Set<String> extractCodes(String fileName) throws IOException {
+        // Load the file from resources
+        File file = new File(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).getFile());
+
+        // Parse JSON using Jackson
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(file);
+
+        // Extract codes from the "resource" array
+        Set<String> codes = new HashSet<>();
+        if (rootNode.has("resource")) {
+            for (JsonNode resourceNode : rootNode.get("resource")) {
+                if (resourceNode.has("code")) {
+                    codes.add(resourceNode.get("code").asText());
+                }
+            }
+        }
+        return codes;
     }
 
 }
--- src/test/java/de/medizininformatikinitiative/torch/service/CrtdlValidatorServiceTest.java
+++ src/test/java/de/medizininformatikinitiative/torch/service/CrtdlValidatorServiceTest.java
@@ -9,6 +9,7 @@ import de.medizininformatikinitiative.torch.model.crtdl.DataExtraction;
 import de.medizininformatikinitiative.torch.setup.IntegrationTestSetup;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -16,12 +17,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CrtdlValidatorServiceTest {
     private final IntegrationTestSetup itSetup = new IntegrationTestSetup();
+    private final CrtdlValidatorService validatorService = new CrtdlValidatorService(itSetup.structureDefinitionHandler());
+    JsonNode node = JsonNodeFactory.instance.objectNode();
 
+    CrtdlValidatorServiceTest() throws IOException {
+    }
 
     @Test
-    void unknownProfile() {
-        CrtdlValidatorService validatorService = new CrtdlValidatorService(itSetup.structureDefinitionHandler());
-        JsonNode node = JsonNodeFactory.instance.objectNode();
+    void unknownProfile() throws IOException {
         Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(new AttributeGroup("unknown.test", List.of(), List.of()))));
 
         assertThatThrownBy(() -> {
@@ -31,16 +34,23 @@ class CrtdlValidatorServiceTest {
     }
 
     @Test
-    void unknownAttribute() {
-        CrtdlValidatorService validatorService = new CrtdlValidatorService(itSetup.structureDefinitionHandler());
-        JsonNode node = JsonNodeFactory.instance.objectNode();
+    void unknownAttribute() throws IOException {
         Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(new AttributeGroup("https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab", List.of(new Attribute("Condition.unknown", false)), List.of()))));
-
 
         assertThatThrownBy(() -> {
             validatorService.validate(crtdl);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Unknown Attributes in https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab");
+    }
+
+    @Test
+    void outsidePatientComparment() throws IOException {
+        Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(new AttributeGroup("https://www.medizininformatik-initiative.de/fhir/core/modul-medikation/StructureDefinition/wirkstofftyp", List.of(), List.of()))));
+
+        assertThatThrownBy(() -> {
+            validatorService.validate(crtdl);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Attribute would lead to direct extraction of a Resources outside the patient compartment https://www.medizininformatik-initiative.de/fhir/core/modul-medikation/StructureDefinition/wirkstofftyp");
     }
 
 
