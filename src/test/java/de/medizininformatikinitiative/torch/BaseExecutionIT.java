65d3c3bece3c1e5b713cb2b6c141f0dad032ee75
Julian Gründner <juliangruendner@googlemail.com>
2025-04-23 18:05:25 +0200

    remove .sujbect reference fields from IT test crtdls and fix polling in base execution test

--- src/test/java/de/medizininformatikinitiative/torch/BaseExecutionIT.java
+++ src/test/java/de/medizininformatikinitiative/torch/BaseExecutionIT.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -155,22 +156,29 @@ public class BaseExecutionIT {
         return bundles;
     }
 
-    private static HttpHeaders pollExtractRequest(String crtdlFile) throws IOException {
+    private static HttpHeaders sendExtractRequest(String crtdlFile) throws IOException {
         var crtdl = loadCrtdl(crtdlFile);
         return torchClient.post()
                 .uri("/fhir/$extract-data")
                 .bodyValue(crtdl)
                 .retrieve()
-                .onStatus(status -> !status.is2xxSuccessful(), ClientResponse::createException)
                 .toEntity(String.class)
-                .retryWhen(Retry.fixedDelay(POLL_MAX_RETRIES, POLL_RETRY_DELAY))
                 .map(HttpEntity::getHeaders)
                 .block();
     }
 
+    private static ResponseEntity<String> pollAndGetBundleLocationResponse(String statusUrl) throws IOException {
+
+        return torchClient.get().uri(statusUrl).retrieve()
+                .onStatus(status -> status.isSameCodeAs(HttpStatusCode.valueOf(202)), ClientResponse::createException)
+                .toEntity(String.class)
+                .retryWhen(Retry.fixedDelay(POLL_MAX_RETRIES, POLL_RETRY_DELAY))
+                .block();
+    }
+
     private static TestUtils.TorchBundleUrls sendCrtdlAndGetOutputUrls(String crtdlFile) throws IOException {
-        var statusUrl = pollExtractRequest(crtdlFile).get("Content-Location").getFirst();
-        var bundleLocationResponse = torchClient.get().uri(statusUrl).retrieve().toEntity(String.class).block();
+        var statusUrl = sendExtractRequest(crtdlFile).get("Content-Location").getFirst();
+        var bundleLocationResponse = pollAndGetBundleLocationResponse(statusUrl);
         return getOutputUrls(bundleLocationResponse);
     }
 
@@ -186,7 +194,7 @@ public class BaseExecutionIT {
         var patientBundles = fetchBundles(bundleUrls.patientBundles());
 
         assertThat(coreBundle).containsNEntries(0);
-        assertThat(patientBundles).hasSize(2);
+        assertThat(patientBundles).hasSize(3);
 
         // test if referenced IDs match the actual patient's IDs
         assertThat(patientBundles).allSatisfy(bundle -> assertThat(bundle).extractOnlyPatient().satisfies(patient ->
