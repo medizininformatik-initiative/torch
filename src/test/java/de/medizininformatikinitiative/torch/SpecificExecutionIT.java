40e610c6b51058325c62113d43dc7219dfc3a7e4
Alexander Kiel <alexanderkiel@gmx.net>
2025-05-10 10:02:06 +0200

    Add Performance Tests
    Closes: #204

--- src/test/java/de/medizininformatikinitiative/torch/SpecificExecutionIT.java
+++ src/test/java/de/medizininformatikinitiative/torch/SpecificExecutionIT.java
@@ -54,7 +54,7 @@ class SpecificExecutionIT {
         logger.info("Uploading test data...for test {}", testName);
         var testDataFolder = "testdata/shorthand/fsh-generated/resources/";
         var testBundle = testDataFolder + "Bundle-test-" + testName + ".json";
-        blazeClient.transact(Files.readString(Path.of(testBundle)));
+        blazeClient.transact(Files.readString(Path.of(testBundle))).block();
     }
 
     public void executeStandardTests(List<Bundle> patientBundles) {
@@ -89,9 +89,12 @@ class SpecificExecutionIT {
         var testName = "diag-enc-diag";
         uploadTestData(testName);
 
-        var statusUrl = torchClient.executeExtractData(TestUtils.loadCrtdl("CRTDL_test_" + testName + ".json"));
+        var statusUrl = torchClient.executeExtractData(TestUtils.loadCrtdl("CRTDL_test_" + testName + ".json")).block();
+        assertThat(statusUrl).isNotNull();
+
         // TODO: remove replacing /fhir if #220 is fixed
-        var statusResponse = torchClient.pollStatus(statusUrl.replace("/fhir", ""));
+        var statusResponse = torchClient.pollStatus(statusUrl.replace("/fhir", "")).block();
+        assertThat(statusResponse).isNotNull();
 
         var coreBundles = statusResponse.coreBundleUrl().stream().flatMap(fileServerClient::fetchBundles).toList();
         var patientBundles = statusResponse.patientBundleUrls().stream().flatMap(fileServerClient::fetchBundles).toList();
@@ -136,9 +139,12 @@ class SpecificExecutionIT {
     void testMustHaveResolve() throws IOException {
         var testName = "diag-no-enc-diag";
         uploadTestData(testName);
-        var statusUrl = torchClient.executeExtractData(TestUtils.loadCrtdl("CRTDL_test_" + testName + ".json"));
+        var statusUrl = torchClient.executeExtractData(TestUtils.loadCrtdl("CRTDL_test_" + testName + ".json")).block();
+        assertThat(statusUrl).isNotNull();
+
         // TODO: remove replacing /fhir if #220 is fixed
-        var statusResponse = torchClient.pollStatus(statusUrl.replace("/fhir", ""));
+        var statusResponse = torchClient.pollStatus(statusUrl.replace("/fhir", "")).block();
+        assertThat(statusResponse).isNotNull();
 
         var coreBundles = statusResponse.coreBundleUrl().stream().flatMap(fileServerClient::fetchBundles).toList();
         var patientBundles = statusResponse.patientBundleUrls().stream().flatMap(fileServerClient::fetchBundles).toList();
