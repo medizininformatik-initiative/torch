41f118f9c9cf4ab22890f64871a22836dcc1e4b1
Alexander Kiel <alexanderkiel@gmx.net>
2025-05-27 15:13:49 +0200

    Remove Unused FHIR Classes
    That classes were copied from FLARE and are no longer used in TORCH.

--- src/main/java/de/medizininformatikinitiative/torch/model/fhir/Entry.java
+++
@@ -1,8 +0,0 @@
-package de.medizininformatikinitiative.torch.model.fhir;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Entry(Resource resource) {
-
-}
