27f8773013217ee9cd5f65f8fbfd39b2a4dc3d83
Lucas Triefenbach <zoo.triefenbach@gmx.de>
2025-05-30 11:13:35 +0200

    Change Consent Key Parsing To Enum

--- src/main/java/de/medizininformatikinitiative/torch/consent/ConsentCodeMapper.java
+++ src/main/java/de/medizininformatikinitiative/torch/consent/ConsentCodeMapper.java
@@ -2,10 +2,19 @@ package de.medizininformatikinitiative.torch.consent;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.medizininformatikinitiative.torch.exceptions.ValidationException;
+import de.medizininformatikinitiative.torch.model.mapping.ConsentKey;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 /**
@@ -13,42 +22,46 @@ import java.util.*;
  */
 public class ConsentCodeMapper {
 
-    private final Map<String, List<String>> consentMap;
+    private final Map<ConsentKey, List<String>> consentMap;
     private final ObjectMapper objectMapper;
 
 
     public ConsentCodeMapper(String consentFilePath, ObjectMapper objectMapper) throws IOException {
-        this.consentMap = new HashMap<>();
+        this.consentMap = new EnumMap<>(ConsentKey.class);
         this.objectMapper = objectMapper;
         buildConsentMap(consentFilePath);
     }
 
     // Method to build the map based on the JSON file
     private void buildConsentMap(String filePath) throws IOException {
-        //Class get Resource as Stream
-        //init method
         File file = new File(filePath);
         JsonNode consentMappingData = objectMapper.readTree(file.getAbsoluteFile());
         for (JsonNode consent : consentMappingData) {
-            String keyCode = consent.get("key").get("code").asText();
-            List<String> relevantCodes = new ArrayList<>();
+            try {
+                ConsentKey keyCode = ConsentKey.fromString(consent.get("key").get("code").asText());
+                List<String> relevantCodes = new ArrayList<>();
 
-            JsonNode fixedCriteria = consent.get("fixedCriteria");
-            if (fixedCriteria != null) {
-                for (JsonNode criterion : fixedCriteria) {
-                    Iterator<JsonNode> values = criterion.get("value").elements();
-                    while (values.hasNext()) {
-                        JsonNode value = values.next();
-                        relevantCodes.add(value.get("code").asText());
+                JsonNode fixedCriteria = consent.get("fixedCriteria");
+                if (fixedCriteria != null) {
+                    for (JsonNode criterion : fixedCriteria) {
+                        Iterator<JsonNode> values = criterion.get("value").elements();
+                        while (values.hasNext()) {
+                            JsonNode value = values.next();
+                            relevantCodes.add(value.get("code").asText());
+                        }
                     }
                 }
+                consentMap.put(keyCode, relevantCodes);
+            } catch (ValidationException e) {
+                throw new IllegalStateException("Consent map has invalid key" + e.getMessage());
             }
-
-            consentMap.put(keyCode, relevantCodes);
+        }
+        if (consentMap.size() != ConsentKey.values().length) {
+            throw new IllegalStateException("Consent map size does not match ConsentKey enum size");
         }
     }
 
-    public Set<String> getRelevantCodes(String key) {
+    public Set<String> getRelevantCodes(ConsentKey key) {
         return new HashSet<>(consentMap.getOrDefault(key, Collections.emptyList()));
     }
 }
--- src/test/java/de/medizininformatikinitiative/torch/service/CrtdlValidatorServiceTest.java
+++ src/test/java/de/medizininformatikinitiative/torch/service/CrtdlValidatorServiceTest.java
@@ -1,24 +1,84 @@
 package de.medizininformatikinitiative.torch.service;
 
 import ca.uhn.fhir.context.FhirContext;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import de.medizininformatikinitiative.torch.exceptions.ValidationException;
 import de.medizininformatikinitiative.torch.management.CompartmentManager;
-import de.medizininformatikinitiative.torch.model.crtdl.*;
+import de.medizininformatikinitiative.torch.model.crtdl.Attribute;
+import de.medizininformatikinitiative.torch.model.crtdl.AttributeGroup;
+import de.medizininformatikinitiative.torch.model.crtdl.Code;
+import de.medizininformatikinitiative.torch.model.crtdl.Crtdl;
+import de.medizininformatikinitiative.torch.model.crtdl.DataExtraction;
+import de.medizininformatikinitiative.torch.model.crtdl.Filter;
 import de.medizininformatikinitiative.torch.model.crtdl.annotated.AnnotatedAttribute;
+import de.medizininformatikinitiative.torch.model.mapping.ConsentKey;
 import de.medizininformatikinitiative.torch.setup.IntegrationTestSetup;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class CrtdlValidatorServiceTest {
+
+    String invalidConsentKeyJson = """
+            {
+                "version": "http://to_be_decided.com/draft-1/schema#",
+                "display": "",
+                "inclusionCriteria": [
+                  [
+                    {
+                      "context": {
+                        "code": "Einwilligung",
+                        "display": "Einwilligung",
+                        "system": "fdpg.mii.cds",
+                        "version": "1.0.0"
+                      },
+                      "termCodes": [
+                        {
+                          "code": "invalid",
+                          "display": "Verteilte, EU-DSGVO konforme Analyse, ohne Krankenassendaten, und mit Rekontaktierung",
+                          "system": "fdpg.consent.combined"
+                        }
+                      ]
+                    }
+                  ]
+                ]
+            }
+            """;
+
+    String validConsentKeyJson = """
+            {
+            "inclusionCriteria": [
+                  [
+                    {
+                      "context": {
+                        "code": "Einwilligung",
+                        "display": "Einwilligung",
+                        "system": "fdpg.mii.cds",
+                        "version": "1.0.0"
+                      },
+                      "termCodes": [
+                        {
+                          "code": "yes-yes-no-yes",
+                          "display": "Verteilte, EU-DSGVO konforme Analyse, ohne Krankenassendaten, und mit Rekontaktierung",
+                          "system": "fdpg.consent.combined"
+                        }
+                      ]
+                    }
+                  ]
+                ]
+            }
+            """;
+
     private final IntegrationTestSetup itSetup = new IntegrationTestSetup();
     private static final FilterService filterService = new FilterService(FhirContext.forR4(), "search-parameters.json");
     private final CrtdlValidatorService validatorService = new CrtdlValidatorService(itSetup.structureDefinitionHandler(),
@@ -36,56 +96,65 @@ class CrtdlValidatorServiceTest {
     }
 
     @Test
-    void unknownProfile() throws ValidationException {
+    void unknownConsentKey() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(invalidConsentKeyJson);
+        Crtdl crtdl = new Crtdl(root, new DataExtraction(List.of(patientGroup, new AttributeGroup("test", "https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab", List.of(), List.of()))));
+        assertThatThrownBy(() -> validatorService.validate(crtdl)).isInstanceOf(ValidationException.class)
+                .hasMessageContaining("No valid Consent Key Found");
+
+    }
+
+
+    @Test
+    void unknownProfile() {
         Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(patientGroup, new AttributeGroup("test", "unknown.test", List.of(), List.of()))));
 
-        assertThatThrownBy(() -> {
-            validatorService.validate(crtdl);
-        }).isInstanceOf(ValidationException.class)
+        assertThatThrownBy(() -> validatorService.validate(crtdl)).isInstanceOf(ValidationException.class)
                 .hasMessageContaining("Unknown Profile: unknown.test");
 
     }
 
 
     @Test
-    void unknownAttribute() throws ValidationException {
+    void unknownAttribute() {
         Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(patientGroup, new AttributeGroup("test", "https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab", List.of(new Attribute("Condition.unknown", false)), List.of()))));
 
-        assertThatThrownBy(() -> {
-            validatorService.validate(crtdl);
-        }).isInstanceOf(ValidationException.class)
+        assertThatThrownBy(() -> validatorService.validate(crtdl)).isInstanceOf(ValidationException.class)
                 .hasMessageContaining("Unknown Attribute Condition.unknown in group test");
 
     }
 
     @Test
-    void referenceWithoutLinkedGroups() throws ValidationException {
+    void referenceWithoutLinkedGroups() {
         Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(patientGroup, new AttributeGroup("test", "https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab", List.of(new Attribute("Observation.subject", false)), List.of()))));
 
-        assertThatThrownBy(() -> {
-            validatorService.validate(crtdl);
-        }).isInstanceOf(ValidationException.class)
+        assertThatThrownBy(() -> validatorService.validate(crtdl)).isInstanceOf(ValidationException.class)
                 .hasMessageContaining("Reference Attribute Observation.subject without linked Groups in group test");
 
     }
 
     @Test
-    void validInput() throws ValidationException {
-        Crtdl crtdl = new Crtdl(node, new DataExtraction(List.of(patientGroup, new AttributeGroup("test", "https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab", List.of(), List.of(new Filter("token", "code", List.of(new Code("some-system", "some-code"))))))));
+    void validInput() throws ValidationException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(validConsentKeyJson);
+        Crtdl crtdl = new Crtdl(root, new DataExtraction(List.of(patientGroup, new AttributeGroup("test", "https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab", List.of(), List.of(new Filter("token", "code", List.of(new Code("some-system", "some-code"))))))));
 
-        assertThat(validatorService.validate(crtdl).dataExtraction().attributeGroups().get(0).attributes()).isEqualTo(
+        var result = validatorService.validate(crtdl);
+
+        assertThat(result.dataExtraction().attributeGroups().get(0).attributes()).isEqualTo(
                 List.of(
                         new AnnotatedAttribute("Patient.id", "Patient.id", "Patient.id", false),
                         new AnnotatedAttribute("Patient.meta.profile", "Patient.meta.profile", "Patient.meta.profile", false)
                 ));
 
-        assertThat(validatorService.validate(crtdl).dataExtraction().attributeGroups().get(1).attributes()).isEqualTo(
+        assertThat(result.dataExtraction().attributeGroups().get(1).attributes()).isEqualTo(
                 List.of(
                         new AnnotatedAttribute("Observation.id", "Observation.id", "Observation.id", false),
                         new AnnotatedAttribute("Observation.meta.profile", "Observation.meta.profile", "Observation.meta.profile", false),
                         new AnnotatedAttribute("Observation.subject", "Observation.subject", "Observation.subject", false, List.of("patientGroupId"))
                 ));
-
+        assertThat(result.consentKey()).isEqualTo(Optional.of(ConsentKey.YES_YES_NO_YES));
     }
 
 
