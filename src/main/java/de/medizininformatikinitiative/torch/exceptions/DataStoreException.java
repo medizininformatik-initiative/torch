8fba52dcd1921e85acc64ceda4d78d3759729057
Lucas Triefenbach <zoo.triefenbach@gmx.de>
2025-11-05 12:39:46 +0100

    Add handling=strict to Fhir Search Calls
    closes https://github.com/medizininformatik-initiative/torch/issues/599

---
+++ src/main/java/de/medizininformatikinitiative/torch/exceptions/DataStoreException.java
@@ -0,0 +1,7 @@
+package de.medizininformatikinitiative.torch.exceptions;
+
+public class DataStoreException extends Exception {
+    public DataStoreException(String errorMessage) {
+        super(errorMessage);
+    }
+}
