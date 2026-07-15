ce09e960171ed2794082bdffcd337ad8e0bd067b
Lucas0T <Lucas0T@users.noreply.github.com>
2026-07-15 10:37:12 +0000

    deploy: eac62c80aac388aff6b49a365915e1b5aca1fde2

---
+++ preview/pr-1067/assets/chunks/channel._msgfKKS.js
@@ -0,0 +1,1 @@
+import{U as a,F as n}from"../app.Dn9y4nrQ.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ preview/pr-1067/assets/chunks/swimlanesDiagram-G3AALYLV.PM6TFL2H.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.D8oz_I5n.js";import{_ as a}from"../app.Dn9y4nrQ.js";import"./chunk-5VM5RSS4.BCPQiulG.js";import"./chunk-XXDRQBXY.D7yCKw1p.js";import"./chunk-VR4S4FIN.uw7YroxO.js";import"./chunk-32BRIVSS.CxZffDY_.js";import"./channel._msgfKKS.js";import"./framework.9SZO4JrK.js";import"./theme.u0pZYko7.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
