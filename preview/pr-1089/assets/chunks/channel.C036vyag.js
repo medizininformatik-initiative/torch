3ececc1f218ac88770cf2d7adaab7d817102bde0
bastianschaffer <bastianschaffer@users.noreply.github.com>
2026-07-22 13:02:54 +0000

    deploy: 93bde59f613b67a78e7de9773a30557c81a33e53

---
+++ preview/pr-1089/assets/chunks/channel.C036vyag.js
@@ -0,0 +1,1 @@
+import{U as a,F as n}from"../app.Bw0ReHDY.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ preview/pr-1089/assets/chunks/swimlanesDiagram-G3AALYLV.C_a28Msa.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.WGqaadW4.js";import{_ as a}from"../app.Bw0ReHDY.js";import"./chunk-5VM5RSS4.B2o9pgaX.js";import"./chunk-XXDRQBXY.Ceiwq2PU.js";import"./chunk-VR4S4FIN.C2DPH49F.js";import"./chunk-32BRIVSS.4TbQycMq.js";import"./channel.C036vyag.js";import"./framework.DhsbzC9a.js";import"./theme.gikBYXCR.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
