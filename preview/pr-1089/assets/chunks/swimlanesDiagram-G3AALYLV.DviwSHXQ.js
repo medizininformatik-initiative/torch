55627a771abb83aa6b6583aef1e921b56b4b774c
bastianschaffer <bastianschaffer@users.noreply.github.com>
2026-07-23 10:14:04 +0000

    deploy: a6f95419f78bde2bd1e6bba5fca527d50f5442cd

---
+++ preview/pr-1089/assets/chunks/swimlanesDiagram-G3AALYLV.DviwSHXQ.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.ustl8w6j.js";import{_ as a}from"../app.BTnsTI-E.js";import"./chunk-5VM5RSS4.Bblnt6eH.js";import"./chunk-XXDRQBXY.Col_5yNx.js";import"./chunk-VR4S4FIN._EHYULPS.js";import"./chunk-32BRIVSS.C5SssVWH.js";import"./channel.B0VkHOOi.js";import"./framework.DhsbzC9a.js";import"./theme.NPRZSXEG.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
