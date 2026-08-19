13f85452b7394356f9903dcce2c49f878271f6ba
Lucas0T <Lucas0T@users.noreply.github.com>
2026-08-19 10:46:14 +0000

    deploy: bd661df7142629e8c233af881b999b147619e3c5

---
+++ preview/pr-1211/assets/chunks/swimlanesDiagram-ULZ7WXOC.BsAA1YK_.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DYJxBkM9.js";import{_ as a}from"../app.BYB48EO1.js";import"./chunk-5VM5RSS4.xyIdef6q.js";import"./chunk-XXDRQBXY.D8Ea3pIQ.js";import"./chunk-KBJHAD2P.BqqIymDz.js";import"./chunk-2GRJ4B5K.C00pfhCi.js";import"./channel.BX0zLRHx.js";import"./framework.M9gi3hut.js";import"./theme.BAWGeDjm.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
